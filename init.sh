d0b6d1016493402d105771aeb7dd8702d455983d
Okada Shohei <okashoicircus0@gmail.com>
2020-08-02 16:07:08 +0900

    index を追加

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+mysql isucon -u isucon -pisucon <<EOF
+CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`,`created_at`);
+CREATE INDEX `memos_idx_user_created_at` ON memos (`user`,`created_at`);
+CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`,`is_private`,`created_at`);
+EOF
