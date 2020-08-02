27306373e838738ce97cedf513500b619810fa3e
Okada Shohei <okashoicircus0@gmail.com>
2020-08-02 16:13:42 +0900

    ` ` で囲っていてエラーになったので削除

--- init.sh
+++ init.sh
@@ -2,8 +2,9 @@
 
 echo 'init process'
 
-mysql isucon -u isucon -pisucon <<EOF
-CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`,`created_at`);
-CREATE INDEX `memos_idx_user_created_at` ON memos (`user`,`created_at`);
-CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`,`is_private`,`created_at`);
+MYSQL="mysql -u isucon -pisucon isucon"
+$MYSQL <<EOF
+CREATE INDEX memos_idx_is_private_created_at ON memos (is_private,created_at);
+CREATE INDEX memos_idx_user_created_at ON memos (user,created_at);
+CREATE INDEX memos_idx_user_is_private_created_at ON memos (user,is_private,created_at);
 EOF
