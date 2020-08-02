d533eb5475c63e0b0716ec00b29729b74bf011c2
Okada Shohei <okashoicircus0@gmail.com>
2020-08-02 19:04:36 +0900

    public_memos テーブルを作成

--- init.sh
+++ init.sh
@@ -8,3 +8,14 @@ CREATE INDEX memos_idx_is_private_created_at ON memos (is_private,created_at);
 CREATE INDEX memos_idx_user_created_at ON memos (user,created_at);
 CREATE INDEX memos_idx_user_is_private_created_at ON memos (user,is_private,created_at);
 EOF
+
+# Migration
+$MYSQL << EOF
+DROP TABLE IF EXISTS public_memos;
+CREATE TABLE public_memos (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  memo int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+INSERT INTO public_memos (memo) SELECT id FROM memos WHERE is_private=0;
+EOF
